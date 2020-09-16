80b0484bf440ced8dca8215b1912cf756e666965
Paul Nguyen <paulngu1120@gmail.com>
2020-09-16 16:54:51 +0000

    Complete pseudoclassical refactor

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -2,15 +2,14 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  var oldStep = this.step;
   // call the old version of step at the beginning of any call to this new version of step
-  oldStep();
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
-makeBlinkyDancer.prototype.step = function() {
+makeBlinkyDancer.prototype.step = function(timeBetweenSteps) {
+  makeDancer.prototype.step.call(this, timeBetweenSteps);
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
--- src/dancer.js
+++ src/dancer.js
@@ -1,9 +1,10 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step(timeBetweenSteps);
+  this.step();
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
@@ -11,10 +12,10 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 };
 
 
-makeDancer.prototype.step = function(timeBetweenSteps) {
+makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
@@ -25,8 +26,5 @@ makeDancer.prototype.setPosition = function(top, left) {
     top: top,
     left: left
   };
-  $(document).ready(function() {
-    $('<span class="dancer Testing"></span>').appendTo('body');
-    this.$node.css(styleSettings);
-  });
+  this.$node.css(styleSettings);
 };
